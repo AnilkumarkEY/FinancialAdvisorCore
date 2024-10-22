0e17305382bee52bfd6af8a2bf138833fe4c1f30
adityaKEY <aditya.kumar@in.ey.com>
2024-10-22 12:51:35 +0530

    create authorization middleware

--- middleware/eventValidation.js
+++ middleware/eventValidation.js
@@ -1,7 +1,13 @@
+const routeValues = require("../config/routesValues");
+const { userProfile } = require("../db");
+const generateUniqueString = require("../utils/generateUniqueString");
 async function eventValidation(request, reply) {
-    // Perform validation logic here
-    const isValid = true;
-    if(!isValid){
+    let eventDefination = routeValues.routeValues[request.url];
+    const isValid = await userProfile.checkValidRoute(request.user.oid,eventDefination);
+    request.isValid = isValid[0];
+    request.isValid["idevent_transaction"] = generateUniqueString();
+    console.log(request.isValid)
+    if(!isValid.length){
         return reply.status(403).send({ message: 'You do not have permission to access this route.' });
     }
   }
