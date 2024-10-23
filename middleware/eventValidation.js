5b06d1fe667376b80ddc2afd1ef6283c8de4bcc7
EY shubham <Shubham.Gupta13@in.ey.com>
2024-10-23 17:20:17 +0530

    fix issue

--- middleware/eventValidation.js
+++ middleware/eventValidation.js
@@ -2,14 +2,25 @@ const routeValues = require("../config/routesValues");
 const { userProfile } = require("../db");
 const generateUniqueString = require("../utils/generateUniqueString");
 async function eventValidation(request, reply) {
-    let eventDefination = routeValues.routeValues[request.url];
+    let eventDefination = routeValues.routeValues[request.url]!==undefined?routeValues.routeValues[request.url]:
+    routeValues.routeValues[makeUrl(request.url)];
+    console.log("eventDefination", eventDefination)
     const isValid = await userProfile.checkValidRoute(request.user.oid,eventDefination);
     request.isValid = isValid[0];
+    console.log("request", request);
     request.isValid["idevent_transaction"] = generateUniqueString();
-    console.log(request.isValid)
+    console.log("request.isValid", request.isValid)
     if(!isValid.length){
         return reply.status(403).send({ message: 'You do not have permission to access this route.' });
     }
+  }
+
+  function makeUrl(urls) {
+    const url = urls;
+    const segments = url.split("/");
+    const desiredSegment = `/${segments[1]}/${segments[2]}`;
+    console.log(desiredSegment);
+    return desiredSegment;
   }
   
 module.exports = { eventValidation };
